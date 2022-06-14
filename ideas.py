2c4071fda0bda4d5bc9faab09e2c8e4b846123ef
Phuong Khau <pkhau0@gmail.com>
2022-06-14 10:31:56 -0500

    Added ideas

---
+++ ideas.py
@@ -0,0 +1,23 @@
+### project ideas
+# Chatbot
+# Card Game
+# animal glossary or quiz game
+# one-piece related game
+# Growth of a zombie virus outbreak
+# Diagnostic imaging scanner 
+# - detecting and classifying tumors based on results of diagnostic tools
+# Hospital or clinic management system
+# - GOAL: check in patients, get them to appropriate place of treament, checkout
+# - Departments: Reception, Radiology, Anesthetics, OR (surgery), ER, Burn Center, ICU, Maternity Ward, Occupational Therapy
+# - Medical Records, Pharmacy,
+
+# CHECKIN #
+# Patient fills out sign-in sheet, hospital associates input data into system, system outputs appropriate department
+# to transfer patient to based on sign-in sheet, also sends to Doctor(s) or Nurse(s) to review and ok it.
+
+# Patient Care #
+# - Room Availability
+# - Patient care status - Waiting (w), Triage (t), Doctor(d), finished (F) and waiting to be discharged
+
+# CHECKOUT, DISCHARGE # 
+# Notes taken on the patient and their treatment, conditions, etc are saved 
